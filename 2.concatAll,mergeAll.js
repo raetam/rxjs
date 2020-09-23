43f927666bf27c77aa39ce377f78eb8db8102ade
raetam <raetamslog@gmail.com@gmail.com>
2020-09-23 15:53:10 +0900

    concatAll::mergeAll::concatMap::mergeMap

---
+++ 2.concatAll,mergeAll.js
@@ -0,0 +1,20 @@
+// lib
+const Rx = require('rxjs')
+const {
+  concatAll,
+  mergeAll,
+  concatMap,
+  mergeMap,
+  take,tap} = require('rxjs/operators')
+
+// stream
+const s1 = Rx.interval(1000).pipe(take(3),tap(console.log))
+const s2 = Rx.interval(1000).pipe(take(3),tap(console.log))
+const s3 = Rx.interval(1000).pipe(take(3),tap(console.log))
+const s4 = Rx.interval(1000).pipe(take(3),tap(console.log))
+const s5 = Rx.interval(1000).pipe(take(3),tap(console.log))
+const s6 = Rx.of(s1,s2,s3,s4,s5)
+s6.pipe(
+  // concatAll()
+  // mergeAll(3)
+).subscribe()
---
+++ 2.concatMap,mergeMap.js
@@ -0,0 +1,30 @@
+// lib
+const Rx = require('rxjs')
+const {concatMap, mergeMap,map,take,tap} = require('rxjs/operators')
+
+// stream
+const s1 = Rx.from(['d1','d2','d3'])
+const s2 = Rx.interval(1000).pipe(take(3),map(d=>d*2),tap(console.log))
+
+// task
+const t1 = (d) => new Promise((resolve,reject)=>setTimeout(()=>{
+  console.log('t1',d)
+  return resolve(d)}, 5000))
+const t2 = (d) => new Promise((resolve,reject)=>setTimeout(()=>{
+  console.log('t2',d)
+  return resolve(d)}, 5000))
+const t3 = (d) => new Promise((resolve,reject)=>setTimeout(()=>{
+  console.log('t3',d)
+  return resolve(d)}, 5000))
+
+async function ts(d){
+  await t1(d)
+  await t2(d)
+  await t3(d)
+}
+
+s2.pipe(
+  mergeMap(d=> Rx.from(ts(d))),
+  // concatMap(d=> Rx.from(ts(d)))
+).subscribe()
+
