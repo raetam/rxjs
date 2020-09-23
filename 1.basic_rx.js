d52b40a4e6858d1dee5816ac8f254780b48bca7b
raetam <raetamslog@gmail.com@gmail.com>
2020-09-23 11:23:01 +0900

    data|>stream|>operators|>subscribe

---
+++ 1.basic_rx.js
@@ -0,0 +1,22 @@
+// lib
+const Rx = require('rxjs')
+const {take,tap,filter,map,reduce} = require('rxjs/operators')
+
+// data
+const d1 = ['d1','d2','d3']
+const d2 = Rx.of('d1','d2','d3')
+const d3 = new Promise((resolve,reject)=>setTimeout(()=>resolve('d1'),100))
+
+// stream
+const s1 = Rx.from(d1)
+const s2 = Rx.from(d2)
+const s_concat = Rx.concat(s1,s2)
+const ss1 = Rx.interval(1000).pipe(take(3))
+const ss2 = Rx.interval(1000).pipe(tap(d=>console.log("스트림에는 영향을 주지않음")))
+const ss_merge = Rx.merge(ss1,ss2)
+
+// subscribe
+ss_merge.subscribe((d) => console.log(d))
+// ss_concat.subscribe((d) => console.log(d))
+// s1.subscribe({ next: (d) => console.log(d),complete: () => 1,error: (e) => e})
+
